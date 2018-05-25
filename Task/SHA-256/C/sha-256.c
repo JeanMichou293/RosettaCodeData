4517ed56730da59cbcc1a997e4d67734a4b08378
JeanMichou293 <JeanMichou293@users.noreply.github.com>
2018-05-25 16:05:03 +0200

    Patch for plowbin compatibility

--- Task/SHA-256/C/sha-256.c
+++ Task/SHA-256/C/sha-256.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <openssl/sha.h>
+#include <unistd.h>
+
+#define INPUT_SIZE 65536
 
 int main (void) {
-	const char *s = "Rosetta code";
+//	const char *s = "Rosetta code";
+	char s[INPUT_SIZE];
+	read(STDIN_FILENO, s, INPUT_SIZE);
 	unsigned char *d = SHA256(s, strlen(s), 0);
-
+/*
 	int i;
 	for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
 		printf("%02x", d[i]);
 	putchar('\n');
-
+*/
 	return 0;
 }
